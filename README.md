e06bf9c216184d67562ef93a6f620d78eeff1d73
Aj0SK <andrejkorman1@gmail.com>
2018-12-23 02:18:35 +0100

    Získavanie basecall retazca zo suboru

---
+++ README.md
@@ -0,0 +1,13 @@
+## prerekvizity
+
+technické: python, pip, virtualenv
+iné: zlib1g-dev
+
+## Použitie
+
+0.  virtualenv env
+
+1.  source env/bin/activate
+    pip install -r requirements.txt
+
+2.  (env) $ python refmatch.py ./test/magnu_20180726_FAH98239_MN19761_sequencing_run_GigFunCapBChi_80071_read_7497_ch_55_strand.fast5
--- refmatch.py
+++ refmatch.py
@@ -1,9 +1,27 @@
+import sys
+import h5py
+import numpy as np
 import mappy as mp
 
-a = mp.Aligner("test/MT-human.fa")  # load or build index
-if not a: raise Exception("ERROR: failed to load/build index")
+if len(sys.argv) != 2:
+    print("Usage is python refmatch.py <sequence>")
+
+sequenceFile = h5py.File(sys.argv[1], 'r')
+
+basecallOut = str(sequenceFile['/Analyses/Basecall_1D_000/BaseCalled_template/Fastq'][()]).split('\\n')
+
+basecallString = basecallOut[1]
+
+print(basecallString)
+
+
+referenceFile = mp.Aligner("test/MT-human.fa")  # load or build index
+if not referenceFile: raise Exception("ERROR: failed to load/build index")
 
 for name, seq, qual in mp.fastx_read("test/MT-orang.fa"): # read a fasta/q sequence
-        for hit in a.map(seq): # traverse alignments
-                print("{}\t{}\t{}\t{}".format(hit.ctg, hit.r_st, hit.r_en, hit.cigar_str))
-                #print(mp.revcomp(seq))
+        for hit in referenceFile.map(seq): # traverse alignments
+            print("{}\t{}\t{}\t{}".format(hit.ctg, hit.r_st, hit.r_en, hit.cigar_str))
+            #s = referenceFile.seq("MT_human", hit.r_st, hit.r_en)    
+            #print(s)
+
+#print(a[i], end = '')
