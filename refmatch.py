5562a78233b81d72dbc9bb7a5b3ffe33a64f5d68
Aj0SK <andrejkorman1@gmail.com>
2018-12-29 22:08:09 +0100

    Zakladna funkcionalita dokoncena

--- refmatch.py
+++ refmatch.py
@@ -6,17 +6,23 @@ import h5py
 import numpy as np
 import mappy as mp
 
-def myF(table, hit_beg, hit_end):
-    x = []
+def myF(sequenceFile, hit_beg, hit_end):
+    basecallEventTable = sequenceFile['/Analyses/Basecall_1D_000/BaseCalled_template/Events'][()]
+    readName = str(list(sequenceFile['Raw/Reads'])[0])
+    rawData = sequenceFile['Raw/Reads/' + readName + "/" + "Signal"][()]
+    
+    out = []
     j = -1
     
-    for index in range(0, len(table)):
-        j = j + table[index][5]
+    for index in range(0, len(basecallEventTable)):
+        j = j + basecallEventTable[index][5]
         if (j>= hit_beg and j<=hit_end) or (j+5> hit_beg and j+5<=hit_end):
-            x.append(table[index][1])
-            print(table[index][4])
+            out.append(rawData[basecallEventTable[index][1].item()].item())
         
-    return x
+    return out
+
+
+
 
 if len(sys.argv) != 3:
     print("Usage is python refmatch.py <reference.fa>")
@@ -24,10 +30,12 @@ if len(sys.argv) != 3:
 # read sequence files and obtain details about it
 
 sequenceFiles = []
+sequenceFilesIndexer = {}
 
 fast5Files = glob.glob(sys.argv[2] + '/**/*.fast5', recursive=True)
 for file in fast5Files:
     sequenceFiles.append(h5py.File(file, 'r'))
+    sequenceFilesIndexer[file] = len(sequenceFilesIndexer)
 
 # create fasta file from sequence string and load it
 
@@ -44,19 +52,18 @@ fastaSequenceFile.close()
 sequenceIndex = mp.Aligner("test/sequence.fa")  
 if not sequenceIndex: raise Exception("ERROR: failed to load/build reference index")
 
-'''
-basecallEventTable = sequenceFile['/Analyses/Basecall_1D_000/BaseCalled_template/Events'][()]
-
-# referenceIndex = mp.Aligner("test/reference.fa")
-# if not referenceIndex: raise Exception("ERROR: failed to load/build reference index")
-'''
+outFile = open("testout/out.txt", "w")
 
 for name, seq, qual in mp.fastx_read(sys.argv[1]): # read a fasta/q sequence
         for hit in sequenceIndex.map(seq): # traverse alignments
-            print("{}\t{}\t{}\t{}".format(hit.ctg, hit.r_st, hit.r_en, hit.cigar_str))
-            #pole = myF(basecallEventTable, hit.r_st, hit.r_en)
-            #print("Pole je" + str(pole))
-            #s = referenceFile.seq("MT_human", hit.r_st, hit.r_en)    
-            #print(s)
+            #print("{}\t{}\t{}".format(hit.ctg, hit.r_st, hit.r_en))
+            index = sequenceFilesIndexer[hit.ctg]
+            pole = myF(sequenceFiles[index], hit.r_st, hit.r_en)
+            outFile.write(hit.ctg)
+            if len(pole) != 0:
+                for elem in pole:
+                    outFile.write(str(elem) + " ")
+                outFile.write("\n")
+                    
 
 os.remove("test/sequence.fa")
