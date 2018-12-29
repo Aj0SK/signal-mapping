7d96c5cf97393b01c72c6e948f2eb0e0b80bfc22
Aj0SK <andrejkorman1@gmail.com>
2018-12-29 19:40:05 +0100

    vyhladavanie vo viacerych suboroch naraz

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ iné: zlib1g-dev
 1.  source env/bin/activate
     pip install -r requirements.txt
 
-2.  (env) $ python refmatch.py ./test/input.txt ./test/magnu_20180726_FAH98239_MN19761_sequencing_run_GigFunCapBChi_80071_read_7497_ch_55_strand.fast5
+2.  (env) $ python refmatch.py ./test/reference.fa test/
--- refmatch.py
+++ refmatch.py
@@ -1,52 +1,62 @@
 import sys
 import os
+import glob
+
 import h5py
 import numpy as np
 import mappy as mp
 
+def myF(table, hit_beg, hit_end):
+    x = []
+    j = -1
+    
+    for index in range(0, len(table)):
+        j = j + table[index][5]
+        if (j>= hit_beg and j<=hit_end) or (j+5> hit_beg and j+5<=hit_end):
+            x.append(table[index][1])
+            print(table[index][4])
+        
+    return x
+
 if len(sys.argv) != 3:
-    print("Usage is python refmatch.py <sequence>")
+    print("Usage is python refmatch.py <reference.fa>")
 
-# get reference string from referenceFile given as first parameter
-referenceFile = open(sys.argv[1], 'r')
-referenceString = referenceFile.read().rstrip('\n')
-referenceFile.close()
+# read sequence files and obtain details about it
 
-# read sequence file and obtain details about it
+sequenceFiles = []
 
-sequenceFile = h5py.File(sys.argv[2], 'r')
-basecallOut = str(sequenceFile['/Analyses/Basecall_1D_000/BaseCalled_template/Fastq'][()]).split('\\n')
-basecallString = basecallOut[1]
-basecallEventTable = sequenceFile['/Analyses/Basecall_1D_000/BaseCalled_template/Events'][()]
-
-# create fasta file from reference string a load it into
-
-fastaReferenceFile = open("test/reference.fa", 'w')
-fastaReferenceFile.write(">ref\n")
-fastaReferenceFile.write(referenceString)
-fastaReferenceFile.close()
-
-referenceIndex = mp.Aligner("test/reference.fa")  
-if not referenceIndex: raise Exception("ERROR: failed to load/build reference index")
+fast5Files = glob.glob(sys.argv[2] + '/**/*.fast5', recursive=True)
+for file in fast5Files:
+    sequenceFiles.append(h5py.File(file, 'r'))
 
 # create fasta file from sequence string and load it
 
 fastaSequenceFile = open("test/sequence.fa", 'w')
-fastaSequenceFile.write(">seq1\n")
-fastaSequenceFile.write(basecallString)
+
+for i in range(0, len(sequenceFiles)):
+    fastaSequenceFile.write(">" + fast5Files[i] + "\n")
+    basecallOut = str(sequenceFiles[i]['/Analyses/Basecall_1D_000/BaseCalled_template/Fastq'][()]).split('\\n')
+    basecallString = basecallOut[1]
+    fastaSequenceFile.write(basecallString + "\n")
+
 fastaSequenceFile.close()
 
-#sequenceIndex = mp.Aligner("test/sequence.fa")  
-#if not sequenceIndex: raise Exception("ERROR: failed to load/build reference index")
+sequenceIndex = mp.Aligner("test/sequence.fa")  
+if not sequenceIndex: raise Exception("ERROR: failed to load/build reference index")
 
-for name, seq, qual in mp.fastx_read("test/sequence.fa"): # read a fasta/q sequence
-        for hit in referenceIndex.map(seq): # traverse alignments
+'''
+basecallEventTable = sequenceFile['/Analyses/Basecall_1D_000/BaseCalled_template/Events'][()]
+
+# referenceIndex = mp.Aligner("test/reference.fa")
+# if not referenceIndex: raise Exception("ERROR: failed to load/build reference index")
+'''
+
+for name, seq, qual in mp.fastx_read(sys.argv[1]): # read a fasta/q sequence
+        for hit in sequenceIndex.map(seq): # traverse alignments
             print("{}\t{}\t{}\t{}".format(hit.ctg, hit.r_st, hit.r_en, hit.cigar_str))
+            #pole = myF(basecallEventTable, hit.r_st, hit.r_en)
+            #print("Pole je" + str(pole))
             #s = referenceFile.seq("MT_human", hit.r_st, hit.r_en)    
             #print(s)
 
-
-# print(a[i], end = '')
-
-os.remove("test/reference.fa")
 os.remove("test/sequence.fa")
