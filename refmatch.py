ecdf609141d53a994106e48de9faa56969300717
Aj0SK <andrejkorman1@gmail.com>
2019-01-15 20:41:58 +0100

    riesenie problemu s file descriptormi

--- refmatch.py
+++ refmatch.py
@@ -6,7 +6,9 @@ import h5py
 import numpy as np
 import mappy as mp
 
-def myF(sequenceFile, hit_beg, hit_end):
+def myF(sequenceFileName, hit_beg, hit_end):
+    sequenceFile = h5py.File(sequenceFileName, 'r')
+    
     basecallEventTable = sequenceFile['/Analyses/Basecall_1D_000/BaseCalled_template/Events'][()]
     readName = str(list(sequenceFile['Raw/Reads'])[0])
     rawData = sequenceFile['Raw/Reads/' + readName + "/" + "Signal"][()]
@@ -18,38 +20,44 @@ def myF(sequenceFile, hit_beg, hit_end):
         j = j + basecallEventTable[index][5]
         if (j>= hit_beg and j<=hit_end) or (j+5> hit_beg and j+5<=hit_end):
             out.append(rawData[basecallEventTable[index][1].item()].item())
-        
+    
+    sequenceFile.close()
     return out
 
-
+################################################################################
 
 
 if len(sys.argv) != 3:
-    print("Usage is python refmatch.py <reference.fa>")
+    print("Usage is python refmatch.py <reference.fa> <search_directory>")
 
-# read sequence files and obtain details about it
+if not os.path.exists("temporary"):
+    os.makedirs("temporary")
 
-sequenceFiles = []
+sequenceFilesNames = []
 sequenceFilesIndexer = {}
-
-fast5Files = glob.glob(sys.argv[2] + '/**/*.fast5', recursive=True)
-for file in fast5Files:
-    sequenceFiles.append(h5py.File(file, 'r'))
-    sequenceFilesIndexer[file] = len(sequenceFilesIndexer)
 
 # create fasta file from sequence string and load it
 
-fastaSequenceFile = open("test/sequence.fa", 'w')
+fastaSequenceFile = open("temporary/sequence.fa", 'w')
 
-for i in range(0, len(sequenceFiles)):
-    fastaSequenceFile.write(">" + fast5Files[i] + "\n")
-    basecallOut = str(sequenceFiles[i]['/Analyses/Basecall_1D_000/BaseCalled_template/Fastq'][()]).split('\\n')
+fast5Files = glob.glob(sys.argv[2] + '/**/*.fast5', recursive=True)
+
+for file in fast5Files:
+    
+    sequenceFilesNames.append(file)
+    sequenceFilesIndexer[file] = len(sequenceFilesIndexer)
+    
+    sequenceFile = h5py.File(file, 'r')
+    basecallOut = str(sequenceFile['/Analyses/Basecall_1D_000/BaseCalled_template/Fastq'][()]).split('\\n')
     basecallString = basecallOut[1]
+    
+    fastaSequenceFile.write(">" + file + "\n")
     fastaSequenceFile.write(basecallString + "\n")
+    sequenceFile.close()
 
 fastaSequenceFile.close()
 
-sequenceIndex = mp.Aligner("test/sequence.fa")  
+sequenceIndex = mp.Aligner("temporary/sequence.fa")  
 if not sequenceIndex: raise Exception("ERROR: failed to load/build reference index")
 
 outFile = open("out.txt", "w")
@@ -58,12 +66,12 @@ for name, seq, qual in mp.fastx_read(sys.argv[1]): # read a fasta/q sequence
         for hit in sequenceIndex.map(seq): # traverse alignments
             #print("{}\t{}\t{}".format(hit.ctg, hit.r_st, hit.r_en))
             index = sequenceFilesIndexer[hit.ctg]
-            pole = myF(sequenceFiles[index], hit.r_st, hit.r_en)
+            pole = myF(sequenceFilesNames[index], hit.r_st, hit.r_en)
             outFile.write(hit.ctg)
             if len(pole) != 0:
                 for elem in pole:
                     outFile.write(" " + str(elem))
                 outFile.write("\n")
-                    
 
-os.remove("test/sequence.fa")
+os.remove("temporary/sequence.fa")
+os.rmdir("temporary")
